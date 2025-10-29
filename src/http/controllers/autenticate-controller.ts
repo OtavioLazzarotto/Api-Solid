f88519745a8ebdeb6076faefd4156d562b57f8d1
Otavin96 <otaviolazzarotto@icloud.com>
2025-10-28 21:45:28 -0300

    First commit

---
+++ src/http/controllers/autenticate-controller.ts
@@ -0,0 +1,30 @@
+import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
+import type { FastifyReply, FastifyRequest } from "fastify";
+import z from "zod";
+import { AuthenticateUseCase } from '../../use-cases/authenticate-use-case';
+import { InvalidCredentialsError } from "@/use-cases/errors/invalid-credentials-error";
+
+export async function authenticate(request: FastifyRequest, reply: FastifyReply) {
+  const authenticateBodySchema = z.object({
+    email: z.email(),
+    password: z.string().min(6),
+  });
+
+  const { email, password } = authenticateBodySchema.parse(request.body);
+
+  try {
+    const prismaUsersRepository = new PrismaUsersRepository();
+    const authenticateUseCase = new AuthenticateUseCase(prismaUsersRepository);
+
+    await authenticateUseCase.execute({
+      email,
+      password,
+    });
+  } catch (error) {
+    if (error instanceof InvalidCredentialsError) {
+      return reply.status(400).send({ message: error.message });
+    }
+  }
+
+  return reply.status(200).send();
+}
---
+++ src/http/controllers/register-controller.ts
@@ -0,0 +1,32 @@
+import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
+import { UserAlreadyExistsError } from "@/use-cases/errors/user-already-exists-error";
+import { RegisterUseCase } from "@/use-cases/register-use-case";
+import type { FastifyReply, FastifyRequest } from "fastify";
+import z from "zod";
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.email(),
+    password: z.string().min(6),
+  });
+
+  const { name, email, password } = registerBodySchema.parse(request.body);
+
+  try {
+    const prismaUsersRepository = new PrismaUsersRepository();
+    const registerUseCase = new RegisterUseCase(prismaUsersRepository);
+
+    await registerUseCase.execute({
+      name,
+      email,
+      password,
+    });
+  } catch (error) {
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: error.message });
+    }
+  }
+
+  return reply.status(201).send();
+}
