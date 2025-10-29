71f3a7e1c05e57260797ddde75daeadc72d6b6cb
OtavioLazzarotto <47401392+OtavioLazzarotto@users.noreply.github.com>
2025-10-29 11:51:13 -0300

    Create use case profile and test

---
+++ src/use-cases/get-users-profile-use-case.ts
@@ -0,0 +1,29 @@
+import { UsersRepository } from "@/repositories/users-repository";
+import { User } from "@prisma/client"
+import { ResourceNotFoundError } from "./errors/resource-not-found-error";
+
+interface GetUsersProfileUseCaseRequest {
+    userId: string;
+}
+
+interface GetUsersProfileUseCaseResponse {
+    user: User;
+}
+
+export class GetUsersProfileUseCase {
+    constructor(private usersRepository: UsersRepository) { }
+
+    async execute({
+        userId
+    }: GetUsersProfileUseCaseRequest): Promise<GetUsersProfileUseCaseResponse> {
+        const user = await this.usersRepository.findById(userId);
+
+        if (!user) {
+            throw new ResourceNotFoundError();
+        }
+
+        return {
+            user,
+        };
+    }
+}
