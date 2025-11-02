3036c95dc8017116e53e15542a529f165fc67ea2
Otavin96 <otaviolazzarotto@icloud.com>
2025-11-02 16:43:08 -0300

    finishing use cases and test

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,25 +1,32 @@
 import { Gym, Prisma } from "@prisma/client";
-import { GymsRepository } from "../gyms-repository";
+import { FindManyNearbyParams, GymsRepository } from "../gyms-repository";
 import { randomUUID } from "node:crypto";
+import { getDistanceBetweenCoordinates } from "@/use-cases/utils/getDistanceBetweenCoordinates";
 
 export class InMemoryGymsRepository implements GymsRepository {
   public items: Gym[] = [];
 
   async create(data: Prisma.GymCreateInput): Promise<Gym> {
-        const gym = {
-          id: data.id ?? randomUUID(),
-          title: data.title,
-          description: data.description ?? null,
-          phone: data.phone ?? null,
-          latitude: new Prisma.Decimal(data.latitude.toString()), 
-          longitude: new Prisma.Decimal(data.longitude.toString()),
-          created_at: new Date(),
-        };
-  
-        this.items.push(gym);
-  
-        return gym;
-      }
+    const gym = {
+      id: data.id ?? randomUUID(),
+      title: data.title,
+      description: data.description ?? null,
+      phone: data.phone ?? null,
+      latitude: new Prisma.Decimal(data.latitude.toString()),
+      longitude: new Prisma.Decimal(data.longitude.toString()),
+      created_at: new Date(),
+    };
+
+    this.items.push(gym);
+
+    return gym;
+  }
+
+  async searchMany(query: string, page: number) {
+    return this.items
+      .filter((item) => item.title.includes(query))
+      .slice((page - 1) * 20, page * 20);
+  }
 
   async findById(id: string): Promise<Gym | null> {
     const gym = this.items.find((item) => item.id == id);
@@ -29,5 +36,19 @@ export class InMemoryGymsRepository implements GymsRepository {
     }
 
     return gym;
+  }
+
+  async findManyNearby(params: FindManyNearbyParams) {
+    return this.items.filter((item) => {
+      const distance = getDistanceBetweenCoordinates(
+        { latitude: params.latitude, longitude: params.longitude },
+        {
+          latitude: item.latitude.toNumber(),
+          longitude: item.longitude.toNumber(),
+        }
+      );
+
+      return distance < 10;
+    });
   }
 }
